819d22acd3660c3e991ffe134025eca1d1161eb6
Yunus Emre Baktıroğlu <yunusemrebaktiroglu@gmail.com>
2023-01-08 21:06:20 +0300

    Project Start

---
+++ src/main/java/com/example/learning/spring/Application.java
@@ -0,0 +1,15 @@
+package com.example.learning.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "learning.spring")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
